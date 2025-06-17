7ab3d976559b94f408e64b8adf333e8988e1f053
skeeji <quentin.luksar@gmail.com>
2025-06-17 16:17:40 +0200

    Create gridfs.ts

---
+++ lib/gridfs.ts
@@ -0,0 +1,21 @@
+// Fichier : lib/gridfs.ts
+
+import clientPromise from "./mongodb"; // On utilise VOTRE fichier de connexion
+import { GridFSBucket } from "mongodb";
+
+let bucket: GridFSBucket;
+
+// Cette fonction prépare et retourne le "seau" de stockage
+export async function getBucket() {
+  if (bucket) {
+    return bucket;
+  }
+
+  const client = await clientPromise;
+  
+  // Important : client.db() utilise la base de données définie dans votre MONGODB_URI
+  const db = client.db(); 
+  
+  bucket = new GridFSBucket(db, { bucketName: 'images' });
+  return bucket;
+}
