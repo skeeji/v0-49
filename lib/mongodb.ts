62bb28de279ecbd5314ed10338801c3c8f8fe024
skeeji <quentin.luksar@gmail.com>
2025-06-26 00:47:38 +0200

    Update mongodb.ts

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -12,18 +12,15 @@ let client: MongoClient
 let clientPromise: Promise<MongoClient>
 
 if (process.env.NODE_ENV === 'development') {
-  // En mode développement, on utilise une variable globale pour ne pas recréer la connexion à chaque rechargement.
   let globalWithMongo = global as typeof globalThis & {
     _mongoClientPromise?: Promise<MongoClient>
   }
-
   if (!globalWithMongo._mongoClientPromise) {
     client = new MongoClient(uri, options)
     globalWithMongo._mongoClientPromise = client.connect()
   }
   clientPromise = globalWithMongo._mongoClientPromise
 } else {
-  // En mode production, on crée une nouvelle connexion.
   client = new MongoClient(uri, options)
   clientPromise = client.connect()
 }
