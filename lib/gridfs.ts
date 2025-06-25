370b912920f14b8a2a38220fc82f2193e5a33837
skeeji <quentin.luksar@gmail.com>
2025-06-26 00:47:53 +0200

    Update gridfs.ts

--- lib/gridfs.ts
+++ lib/gridfs.ts
@@ -2,25 +2,22 @@
 import { GridFSBucket } from "mongodb";
 import clientPromise from "./mongodb";
 
-const DBNAME = "gersaint"; // Centralisons le nom de la base de données
-const BUCKET_NAME = "media"; // Un nom unique pour notre bucket de fichiers
+const DBNAME = "gersaint";
+const BUCKET_NAME = "media";
 
 let bucket: GridFSBucket;
 
-// Fonction pour initialiser et/ou récupérer l'instance du bucket GridFS
 export const getBucket = async (): Promise<GridFSBucket> => {
   if (bucket) {
     return bucket;
   }
-
   try {
     const client = await clientPromise;
     const db = client.db(DBNAME);
     bucket = new GridFSBucket(db, { bucketName: BUCKET_NAME });
-    console.log(`✅ GridFS Bucket '${BUCKET_NAME}' initialisé.`);
     return bucket;
   } catch (error) {
-    console.error("❌ Erreur lors de l'initialisation de GridFS Bucket:", error);
-    throw new Error("Impossible d'initialiser le stockage de fichiers GridFS.");
+    console.error("❌ Erreur initialisation GridFS:", error);
+    throw new Error("Impossible d'initialiser le stockage GridFS.");
   }
 };
